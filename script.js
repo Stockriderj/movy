546f4f4735ab3b04bf26f95e9240b5a45a99aa0c
Stockriderj <78283807+Stockriderj@users.noreply.github.com>
2021-05-16 17:03:34 -0500

    Add files via upload
    Simplified code a little, tinkered with the animations and made the movy move faster.

--- script.js
+++ script.js
@@ -3,40 +3,69 @@ let y = 0;
 let x = 0;
 let width = 100;
 let height = 100;
+const changeYBy = 50;
+const changeXBy = 50;
+const changeWidthBy = 10;
+const changeHeightBy = 10;
+
+function changeMovyProperty(type, property, changeBy) {
+    switch (type) {
+        case "position":
+
+            switch (property) {
+                case "x":
+                    x += changeBy;
+                    blue.style.right = `${x}px`;
+                    break;
+                case "y":
+                    y += changeBy;
+                    blue.style.bottom = `${y}px`;
+            }
+
+            break;
+        case "size":
+
+            switch (property) {
+                case "width":
+                    width += changeBy;
+                    blue.style.width = `${width}px`;
+                    break;
+                case "height":
+                    height += changeBy;
+                    blue.style.height = `${height}px`;
+            }
+
+            break;
+
+    }
+
+}
 
 window.addEventListener('keydown', (e) => {
     switch (e.code) {
         case 'KeyW':
-            y += 10;
-            blue.style.bottom = `${y}px`;
+            changeMovyProperty("position", "y", changeYBy);
             break;
         case 'KeyS':
-            y -= 10;
-            blue.style.bottom = `${y}px`;
+            changeMovyProperty("position", "y", changeYBy - changeYBy * 2);
             break;
         case 'KeyA':
-            x -= 10;
-            blue.style.left = `${x}px`;
+            changeMovyProperty("position", "x", changeXBy);
             break;
         case 'KeyD':
-            x += 10;
-            blue.style.left = `${x}px`;
+            changeMovyProperty("position", "x", changeXBy - changeXBy * 2);
             break;
         case 'ArrowDown':
-            height += 10;
-            blue.style.height = `${height}px`;
+            changeMovyProperty("size", "height", changeHeightBy);
             break;
         case 'ArrowUp':
-            height -= 10;
-            blue.style.height = `${height}px`;
+            changeMovyProperty("size", "height", changeHeightBy - changeHeightBy * 2);
             break;
         case 'ArrowRight':
-            width += 10;
-            blue.style.width = `${width}px`;
+            changeMovyProperty("size", "width", changeWidthBy);
             break;
         case 'ArrowLeft':
-            width -= 10;
-            blue.style.width = `${width}px`;
+            changeMovyProperty("size", "width", changeWidthBy - changeWidthBy * 2);
             break;
     }
 });
